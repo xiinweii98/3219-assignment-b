8b387523fe1421ac8d49428616562dce7afda26c
xiinweii98 <xiinweii98@gmail.com>
2022-10-23 00:03:17 +0800

    add in more tests

--- tests/test.js
+++ tests/test.js
@@ -1,30 +1,91 @@
-let chai = require('chai')
-let chaiHttp = require('chai-http')
-let app = require('../index.js')
-let mongoose = require('mongoose')
-require('dotenv').config()
+let chai = require("chai");
+let chaiHttp = require("chai-http");
+let app = require("../index.js");
+let mongoose = require("mongoose");
+require("dotenv").config();
+let Message = require("../messageModel");
 
-chai.use(chaiHttp)
-chai.should()
+chai.use(chaiHttp);
+chai.should();
 
 describe("Tests for application.", () => {
-    before((done) => {
-        mongoose.connect('mongodb://localhost:27017');
-        db = mongoose.connection;
-        db.on("error", console.error.bind(console, "Unable to connect to MongoDB"));
-        db.once("open", function () {
-          console.log("Connected to MongoDB");
-          done();
-        })
-    })
-    describe("Test case for indexing all messages.", () => {
-        it("Should retrieve all messages in the database.", async() => {
-            chai.request(app)
-                .get('/api/message')
-                .end((err, res) => {
-                    res.should.have.status(201)
-                    res.body.should.be.a('object')
-                })
-        })
-    })
-})
+  before((done) => {
+    mongoose.connect("mongodb://localhost:27017");
+    db = mongoose.connection;
+    db.on("error", console.error.bind(console, "Unable to connect to MongoDB"));
+    db.once("open", function () {
+      console.log("Connected to MongoDB");
+      done();
+    });
+  });
+  describe("Test case for indexing all messages. (GET)", () => {
+    it("Should retrieve all messages in the database.", async () => {
+      chai
+        .request(app)
+        .get("/api/message")
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a("object");
+        });
+    });
+  });
+  describe("Test case for adding a message. (POST)", () => {
+    it("Should add message in the database.", async () => {
+      const message = {
+        name: "xinwei",
+        content: "xinwei1",
+      };
+      chai
+        .request(app)
+        .post("/api/message")
+        .send(message)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.a("object");
+          res.body.should.have.property("name");
+          res.body.should.have.property("content");
+        });
+    });
+  });
+  describe("Test case for updating a message. (PUT)", () => {
+    it("Should update message in the database.", async () => {
+      const message = new Message({
+        name: "xinwei",
+        content: "xinwei",
+      });
+      const newMessage = {
+        name: "xinwei",
+        content: "i am xinwei",
+      };
+      message.save((err, msg) => {
+        chai
+          .request(app)
+          .put("/api/message" + msg.id)
+          .send(newMessage)
+          .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.be.a("object");
+            res.body.should.have.property("name");
+            res.body.should.have.property("content").eql("i am not xinwei");
+          });
+      });
+    });
+  });
+  describe("Test case for deleting a message. (DELETE)", () => {
+    it("Should delete message in the database.", async () => {
+      const message = new Message({
+        name: "xinwei",
+        content: "xinwei",
+      });
+      message.save((err, msg) => {
+        chai
+          .request(app)
+          .delete("/api/message" + msg.id)
+          .end((err, res) => {
+            res.should.have.status(201);
+            res.body.should.have.property("message");
+          });
+      });
+    });
+  });
+});
